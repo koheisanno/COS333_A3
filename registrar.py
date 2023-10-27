da2dc046cb1e47a18da389a303b3223a2be830c4
Jacob B. Colchamiro <jc4557@courselab02.cs.princeton.edu>
2023-10-26 21:18:29 -0400

    fixed cookies

--- registrar.py
+++ registrar.py
@@ -17,61 +17,33 @@ def str_or_empty(arg):
 
 @app.route('/regdetails', methods=['GET'])
 def regdetails():
+    last_dept = flask.request.cookies.get('dept')
+    last_coursenum = flask.request.cookies.get('coursenum')
+    last_area = flask.request.cookies.get('area')
+    last_title = flask.request.cookies.get('title')
+
     classid = flask.request.args.get('classid')
 
     results = reg_db.connect_to_db(classid, 'get_detail')
 
-    html_code = flask.render_template('regdetails.html', results=results, classid = classid)
+    html_code = flask.render_template('regdetails.html', results=results, dept = last_dept, coursenum = last_coursenum,
+                                      area = last_area, title = last_title, classid = classid)
     response = flask.make_response(html_code)
     return response
 
 @app.route('/', methods=['GET'])
 def index():
-    last_dept = flask.request.cookies.get('dept')
-    last_coursenum = flask.request.cookies.get('coursenum')
-    last_area = flask.request.cookies.get('area')
-    last_title = flask.request.cookies.get('title')
     
-    new_dept = flask.request.args.get('dept')
-    if new_dept is not None:
-        dept = new_dept
-    elif last_dept is not None:
-        dept = last_dept
-    else:
-        dept = ''
+    
+    dept = flask.request.args.get('dept')
 
-    new_coursenum = flask.request.args.get('coursenum')
-    if new_coursenum is not None:
-        coursenum = new_coursenum
-    elif last_coursenum is not None:
-        coursenum = last_coursenum
-    else:
-        coursenum = ''
+    coursenum = flask.request.args.get('coursenum')
+
     
-    new_area = flask.request.args.get('area')
-    if new_area is not None:
-        area = new_area
-    elif last_area is not None:
-        area = last_area
-    else:
-        area = ''
+    area = flask.request.args.get('area')
     
-    new_title = flask.request.args.get('title')
-    if new_title is not None:
-        title = new_title
-    elif last_title is not None:
-        title = last_title
-    else:
-        title = ''
+    title = flask.request.args.get('title')
     
-    
-    
-    # if coursenum is None: 
-    #     coursenum = flask.request.cookies.get('coursenum')
-    # if area is None: 
-    #     area = flask.request.cookies.get('area')
-    # if title is None: 
-    #     title = flask.request.cookies.get('title')
 
     results = reg_db.connect_to_db([clean_arg(dept), 
                                     clean_arg(coursenum), 
